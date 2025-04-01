380107f9de27d06fde24e31e249a7a678ad5564d
lok-lyu <lyulelok@gmail.com>
2025-04-01 15:23:42 -0400

    center a div

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,24 +3,22 @@ import { FaGithub } from "react-icons/fa";
 export default function NavBar() {
   return (
     <>
-      <footer className="fixed bottom-0 bg-amber-400 block w-full p-4">
-        <div className="flex justify-between items-center">
-          <div className="block md:hidden text-gray-800 text-sm md:text-lg">
-            Author @ Lok Lyu
-          </div>
-          <div className="hidden md:block text-gray-800 text-sm md:text-lg">
-            Designed and Implemented by Lok Lyu
-          </div>
-          <div className="hidden md:block">
-            <a href="https://github.com/lelyu/tip-calculator">
-              <span className="text-2xl">
-                <FaGithub />
-              </span>
-            </a>
-          </div>
-          <div className="text-gray-800 text-sm md:text-lg">
-            Daily Mood @ 2025
-          </div>
+      <footer className="fixed bottom-0 bg-amber-400 w-full p-4 flex justify-between items-center">
+        <div className="block md:hidden text-gray-800 text-sm md:text-lg">
+          Author @ Lok Lyu
+        </div>
+        <div className="hidden md:block text-gray-800 text-sm md:text-lg">
+          Designed and Implemented by Lok Lyu
+        </div>
+        <div className="hidden md:block absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <a href="https://github.com/lelyu/tip-calculator">
+            <span className="text-2xl">
+              <FaGithub />
+            </span>
+          </a>
+        </div>
+        <div className="text-gray-800 text-sm md:text-lg">
+          Daily Mood @ 2025
         </div>
       </footer>
     </>
