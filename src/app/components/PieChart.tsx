9b5e7f42bca250b42d40fe5bbfc186e46ef6e1b5
lok-lyu <lyulelok@gmail.com>
2025-04-13 14:35:54 -0400

    recreated piechart with proper d3

--- src/app/components/PieChart.tsx
+++ src/app/components/PieChart.tsx
@@ -1,3 +1,48 @@
-export default function PieChart() {
-	return <div>PieChart</div>
+"use client";
+
+import * as d3 from "d3";
+import { useEffect } from "react";
+export default function PieChart({ mockData }) {
+  const moodCountData = mockData.reduce((acc, curr) => {
+    acc[curr.mood] = (acc[curr.mood] || 0) + 1;
+    return acc;
+  }, {});
+
+  const moodCountArr = [];
+  for (const i in moodCountData) {
+    moodCountArr.push({
+      mood: i,
+      count: moodCountData[i],
+    });
+  }
+
+  let pieGenerator = d3.pie().value((d) => d.count);
+  let arcData = pieGenerator(moodCountArr);
+
+  let arcGenerator = d3.arc().innerRadius(20).outerRadius(100);
+
+  const color = d3
+    .scaleOrdinal()
+    .domain(moodCountArr.map((curr) => curr.mood))
+    .range(d3.schemePaired);
+
+  console.log(color("neutral"));
+  useEffect(() => {
+    d3.select(".pie")
+      .selectAll("path")
+      .data(arcData)
+      .enter()
+      .append("path")
+      .attr("d", arcGenerator)
+      .attr("fill", (d) => color(d))
+      .attr("stroke", "white");
+  }, []);
+
+  return (
+    <>
+      <svg className="w-full h-full">
+        <g className="pie" transform="translate(200, 200)"></g>
+      </svg>
+    </>
+  );
 }
--- src/app/vis/page.tsx
+++ src/app/vis/page.tsx
@@ -1,15 +1,17 @@
-import PieChart from '../components/PieChart'
-import moodData from '../../../lib/mockData.json'
+import PieChart from "../components/PieChart";
+import moodData from "../../../lib/mockData.json";
 
 export default function Page() {
-	return (
-		<>
-			<div className='flex flex-col min-h-screen bg-gray-100 p-4'>
-				<h1 className='w-fit text-4xl font-bold mb-4 dark:text-gray-600 border rounded-md p-2'>
-					Visualizations
-				</h1>
-				<PieChart data={moodData} />
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div className="flex flex-col min-h-screen bg-gray-100 p-4">
+        <h1 className="w-fit text-4xl font-bold mb-4 dark:text-gray-600 border rounded-md p-2">
+          Visualizations
+        </h1>
+        <div className="w-96 h-96">
+          <PieChart mockData={moodData} />
+        </div>
+      </div>
+    </>
+  );
 }
